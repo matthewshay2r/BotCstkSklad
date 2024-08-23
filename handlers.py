2ee05c943f8f1a024355f891cbb8c7c4fe88aa26
matthewshay2r <mdshaiturov@edu.hse.ru>
2024-08-23 13:53:47 +0600

    add search in json

--- handlers.py
+++ handlers.py
@@ -3,6 +3,8 @@ from config import *
 from aiogram import types
 from aiogram import F, Router, Bot
 from db import Db
+import json
+import re
 
 API_TOKEN = BOT_TOKEN
 bot = Bot(token=API_TOKEN)
@@ -25,7 +27,31 @@ async def start(message: types.Message):
 
 @router.message(F.text)
 async def ans_message(message: types.Message):
-    await bot.send_message(message.from_user.id, message.text)
+    with open('data/Остатки.json', 'r') as data_file:
+        data = json.load(data_file)["Ячейки"]
+
+    size_input = message.text.strip()
+    size_parts = size_input.replace('-', ' ').split()
+
+    filtered_items = [
+        item for item in data
+        if all(part in item["Номенклатура"] for part in size_parts)
+    ]
+
+    if not filtered_items:
+        await message.reply("По вашему запросу ничего не найдено.")
+        return
+
+    response = ""
+    for item in filtered_items:
+        response += (f"{item['Номенклатура']}\n"
+                     f"Складская ячейка: {item['СкладскаяЯчейка']}\n"
+                     f"Подразделение: {item['Подразделение']}\n"
+                     f"Остаток: {item['КонечныйОстаток']}\n\n\n")
+
+    max_length = 4096
+    for i in range(0, len(response), max_length):
+        await message.reply(response[i:i + max_length])
 
 
 @router.message(Command('help'))
