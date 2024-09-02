b78ac2df98d62019799265d816d6b8b5988f22d0
matthewshay2r <148196025+matthewshay2r@users.noreply.github.com>
2024-09-02 08:09:48 +0300

    add buttons with container and fix lower bug

--- handlers.py
+++ handlers.py
@@ -2,6 +2,7 @@ from aiogram.filters import Command
 from config import *
 from aiogram import types
 from aiogram import F, Router, Bot
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 from db import Db
 import json
 import requests
@@ -11,23 +12,89 @@ bot = Bot(token=API_TOKEN)
 router = Router()
 bot_database = Db('users_db.db')
 
+keyboard_list = [
+    [KeyboardButton(text="Лишние"), KeyboardButton(text="Переделка")],
+    [KeyboardButton(text="У6"), KeyboardButton(text="Хранение")]
+]
+keyboard = ReplyKeyboardMarkup(keyboard=keyboard_list, resize_keyboard=True)
+
 
 @router.message(Command('start'))
 async def start(message: types.Message):
     if not bot_database.check_user(message.from_user.id):
         await bot.send_message(message.from_user.id,
                                text="Привет. Это складской бот компании ЦСТК.\nЯ умею показывать остатки шин по "
-                                    "ячейкам хранения и контейнерах.")
+                                    "ячейкам хранения и контейнерах.", reply_markup=keyboard)
         bot_database.add_user(message.from_user.id, message.from_user.username)
     else:
         await bot.send_message(message.from_user.id,
                                text="Привет. Это складской бот компании ЦСТК.\nЯ умею показывать остатки шин по "
-                                    "ячейкам хранения и контейнерах.")
+                                    "ячейкам хранения и контейнерах.", reply_markup=keyboard)
+
+
+@router.message(F.text == "Лишние")
+async def ans_message(message: types.Message):
+    msg = await bot.send_message(message.from_user.id, "🔄Подождите чуть-чуть")
+    r = requests.get('http://176.67.54.67:777/UT10/hs/tire/case', headers=HEADERS)
+    r.encoding = None
+    if r.status_code == 200:
+        data = json.loads(r.text)["Ячейки"][0]
+        text = f'{data["СкладскаяЯчейка"]}\nКоличествоРасход {data["КоличествоРасход"]}\nКонечныйОстаток {data["КонечныйОстаток"]}'
+        await bot.send_message(message.from_user.id, text, reply_markup=keyboard)
+        await bot.delete_message(message.from_user.id, msg.message_id)
+    else:
+        await message.reply("Произошла ошибка при получение данных.", reply_markup=keyboard)
+        await bot.delete_message(message.from_user.id, msg.message_id)
+
+
+@router.message(F.text == "Переделка")
+async def ans_message(message: types.Message):
+    msg = await bot.send_message(message.from_user.id, "🔄Подождите чуть-чуть")
+    r = requests.get('http://176.67.54.67:777/UT10/hs/tire/case', headers=HEADERS)
+    r.encoding = None
+    if r.status_code == 200:
+        data = json.loads(r.text)["Ячейки"][1]
+        text = f'{data["СкладскаяЯчейка"]}\nКоличествоРасход {data["КоличествоРасход"]}\nКонечныйОстаток {data["КонечныйОстаток"]}'
+        await bot.send_message(message.from_user.id, text, reply_markup=keyboard)
+        await bot.delete_message(message.from_user.id, msg.message_id)
+    else:
+        await message.reply("Произошла ошибка при получение данных.", reply_markup=keyboard)
+        await bot.delete_message(message.from_user.id, msg.message_id)
+
+
+@router.message(F.text == "У6")
+async def ans_message(message: types.Message):
+    msg = await bot.send_message(message.from_user.id, "🔄Подождите чуть-чуть")
+    r = requests.get('http://176.67.54.67:777/UT10/hs/tire/case', headers=HEADERS)
+    r.encoding = None
+    if r.status_code == 200:
+        data = json.loads(r.text)["Ячейки"][2]
+        text = f'{data["СкладскаяЯчейка"]}\nКоличествоРасход {data["КоличествоРасход"]}\nКонечныйОстаток {data["КонечныйОстаток"]}'
+        await bot.send_message(message.from_user.id, text, reply_markup=keyboard)
+        await bot.delete_message(message.from_user.id, msg.message_id)
+    else:
+        await message.reply("Произошла ошибка при получение данных.", reply_markup=keyboard)
+        await bot.delete_message(message.from_user.id, msg.message_id)
+
+
+@router.message(F.text == "Хранение")
+async def ans_message(message: types.Message):
+    msg = await bot.send_message(message.from_user.id, "🔄Подождите чуть-чуть")
+    r = requests.get('http://176.67.54.67:777/UT10/hs/tire/case', headers=HEADERS)
+    r.encoding = None
+    if r.status_code == 200:
+        data = json.loads(r.text)["Ячейки"][3]
+        text = f'{data["СкладскаяЯчейка"]}\nКоличествоРасход {data["КоличествоРасход"]}\nКонечныйОстаток {data["КонечныйОстаток"]}'
+        await bot.send_message(message.from_user.id, text, reply_markup=keyboard)
+        await bot.delete_message(message.from_user.id, msg.message_id)
+    else:
+        await message.reply("Произошла ошибка при получение данных.", reply_markup=keyboard)
+        await bot.delete_message(message.from_user.id, msg.message_id)
 
 
 @router.message(F.text)
 async def ans_message(message: types.Message):
-    msg = await bot.send_message(message.from_user.id,"🔄Подождите чуть-чуть")
+    msg = await bot.send_message(message.from_user.id, "🔄Подождите чуть-чуть")
     r = requests.get(URL, headers=HEADERS)
     r.encoding = None
     if r.status_code == 200:
@@ -38,11 +105,11 @@ async def ans_message(message: types.Message):
 
         filtered_items = [
             item for item in data
-            if all(part in item["Номенклатура"] for part in size_parts)
+            if all(part.lower() in item["Номенклатура"].lower() for part in size_parts)
         ]
 
         if not filtered_items:
-            await message.reply("По вашему запросу ничего не найдено.")
+            await message.reply("По вашему запросу ничего не найдено.", reply_markup=keyboard)
             await bot.delete_message(message.from_user.id, msg.message_id)
             return
 
@@ -55,8 +122,8 @@ async def ans_message(message: types.Message):
 
         max_length = 4096
         for i in range(0, len(response), max_length):
-            await message.reply(response[i:i + max_length])
-            await bot.delete_message(message.from_user.id, msg.message_id)
+            await message.reply(response[i:i + max_length], reply_markup=keyboard)
+        await bot.delete_message(message.from_user.id, msg.message_id)
     else:
-        await message.reply("Произошла ошибка при получение данных.")
+        await message.reply("Произошла ошибка при получение данных.", reply_markup=keyboard)
         await bot.delete_message(message.from_user.id, msg.message_id)
